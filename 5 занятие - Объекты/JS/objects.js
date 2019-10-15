371650c034ac0d40554bc0ed0d34c402d822c289
Alexmtk108 <54139092+Alexmtk108@users.noreply.github.com>
2019-10-15 21:38:37 +0600

    1
    1

---
+++ 5 занятие - Объекты/JS/objects.js
@@ -0,0 +1,21 @@
+let car = {
+	brand: "Toyota",
+	model: "Supra",
+	year: 1996,
+	TypeBody: "coupe",
+	typeGasEngine: "gasoline",
+	powerEngine: 330,
+	volumeEngine: 2997;
+	transmission: "manual",
+	typeWheelDrive: "real",
+	maxSpeeD: 250,
+	accelerate0To100: 5.1,
+	weight: 1585,
+	tankVolume: 80,
+	fuelRate: 11,
+	trunkVolume: 184,
+	tuning: false,
+
+
+
+};
