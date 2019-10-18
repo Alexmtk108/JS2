e6fc44d77750888ebabdddbdda8a39109a3db3b5
Alexmtk108 <54139092+Alexmtk108@users.noreply.github.com>
2019-10-18 12:23:35 +0600

    5

--- 5 занятие - Объекты/JS/objects.js
+++ 5 занятие - Объекты/JS/objects.js
@@ -22,25 +22,25 @@ let car = {
 		// tuningItem = {
 		// 	sub: true,
 		// },
-	howDrive: function() {
-		console.log((this.tankVolume/this.fuelRate)*100);
-		return;
-	},
-	startEngine(){
-		if(car.tankVolume>0.1){
-			console.log("wrom wrum " + car.howDrive());
-		}else{
-			console.log(("oh noo"));
-		}
-	},
-	tun(){
-		car.tuning = true
-		car.maxSpeeD+= 50;
-		car.powerEngine+=100;
-		car.fuelRate=car.fuelRate+6;
-		return;
+	// howDrive: function() {
+	// 	console.log((this.tankVolume/this.fuelRate)*100);
+	// 	return;
+	// },
+	// startEngine(){
+	// 	if(car.tankVolume>0.1){
+	// 		console.log("wrom wrum " + car.howDrive());
+	// 	}else{
+	// 		console.log(("oh noo"));
+	// 	}
+	// },
+	// tun(){
+	// 	car.tuning = true
+	// 	car.maxSpeeD+= 50;
+	// 	car.powerEngine+=100;
+	// 	car.fuelRate=car.fuelRate+6;
+	// 	return;
 		
-	}
+	// }
 }
 let car2 = {
 	brand: "Toyota",
@@ -62,26 +62,48 @@ let car2 = {
 		// tuningItem = {
 		// 	sub: true,
 		// },
-	howDrive: function() {
-		console.log((this.tankVolume/this.fuelRate)*100);
-		return;
-	},
-	startEngine(){
-		if(car.tankVolume>0.1){
-			console.log("wrom wrum " + car.howDrive());
-		}else{
-			console.log(("oh noo"));
-		}
-	},
-	tun(){
-		car.tuning = true
-		car.maxSpeeD+= 50;
-		car.powerEngine+=100;
-		car.fuelRate=car.fuelRate+6;
-		return;
+	// howDrive: function() {
+	// 	console.log((this.tankVolume/this.fuelRate)*100);
+	// 	return;
+	// },
+	// startEngine(){
+	// 	if(car.tankVolume>0.1){
+	// 		console.log("wrom wrum " + car.howDrive());
+	// 	}else{
+	// 		console.log(("oh noo"));
+	// 	}
+	// },
+	// tun(){
+	// 	car.tuning = true
+	// 	car.maxSpeeD+= 50;
+	// 	car.powerEngine+=100;
+	// 	car.fuelRate=car.fuelRate+6;
+	// 	return;
 		
+	// }
+}
+let i = 0, k = 0, result;
+
+for(i in car){
+	let ident = false;
+	// console.log(car[i]);
+	// console.log(i +' '+ car[i]);
+	for(k in car2){
+		// console.log(car2[i])
+		if(i == k){ 
+			ident = true;
+		}
+
+		if (ident==true) {
+			result = 'ident';
+		}
+		if(!ident) {
+			result = 'no ident';
+		}
 	}
+
 }
-if (Object.values(сar) == Object.values(car)){
-	console.log('да');
-}
+console.log(result);
+
+//Сравнивает последнее свойство
+//как сделать значения. не сравнивает значение свойства
